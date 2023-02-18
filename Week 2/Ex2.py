cc7a6c81ff81bab340b68036971f74c13bec5725
SamRudgyardBath <sr2116@bath.ac.uk>
2023-02-18 09:16:56 +0000

    Week 2 Ex 2 - Compare to RK4
    Compare Euler Method, RK2 and RK4 methods for a range of N values.

--- Week 2/Ex2.py
+++ Week 2/Ex2.py
@@ -40,7 +40,26 @@ def SecondOrderRK():
             x0 = x1
         plt.plot(tVals, xVals, 'g', label=f'RK2, N = {N}')
         xVals.clear()
+        
+def FourthOrderRK():
+    for N in nVals:
+        tVals = np.linspace(0, 10, N)
+        x0 = 0
+        h = 10/N
+        for t in tVals:
+            k1 = h*Dxdt(x0, t)
+            k2 = h*Dxdt(x0 + 0.5*k1, t + 0.5*h)
+            k3 = h*Dxdt(x0 + 0.5*k2, t + 0.5*h)
+            k4 = h*Dxdt(x0 + k3, t + h)
+            x1 = x0 + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+            xVals.append(x1)
+            x0 = x1
+        plt.plot(tVals, xVals, 'b', label=f'RK4, N = {N}')
+        xVals.clear()
+
 EulerMethod()
 SecondOrderRK()
+FourthOrderRK()
+plt.title('Comparison of Euler, RK2 and RK4')
 plt.legend()
 plt.show()
