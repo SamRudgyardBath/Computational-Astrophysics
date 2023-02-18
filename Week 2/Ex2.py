5b4df9c8eee27c3ea3431dea63a1942ffffde006
SamRudgyardBath <sr2116@bath.ac.uk>
2023-02-18 09:14:32 +0000

    Week 2 Ex 2 - Second Order RK
    Use 2nd Order Runge-Kutta method to solve the same equation as in Ex 1.

---
+++ Week 2/Ex2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 18:54:12 2023
+
+@author: sam
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Dxdt(x, t):
+    return np.sin(t) - x**3
+
+nVals = [20, 50, 200]
+
+xVals = list()
+
+def SecondOrderRK():
+    for N in nVals:
+        tVals = np.linspace(0, 10, N)
+        x0 = 0
+        h = 10/N
+        for t in tVals:
+            k1 = h*Dxdt(x0, t)
+            k2 = h*Dxdt(x0 + 0.5*k1, t + 0.5*h)
+            x1 = x0 + k2
+            xVals.append(x1)
+            x0 = x1
+        plt.plot(tVals, xVals, 'g', label=f'RK2, N = {N}')
+        xVals.clear()
+SecondOrderRK()
+plt.legend()
+plt.show()
