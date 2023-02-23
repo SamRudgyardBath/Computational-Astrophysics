8ec58549c3b80e78585591879169e115096e05f8
SamRudgyardBath <sr2116@bath.ac.uk>
2023-02-23 14:40:30 +0000

    Week 3 Ex2 - Calculate U, KE and Total E
    Calculate potential energy U, kinetic energy T and total energy E for each time step. Plot these energies as a function of time.

--- Week 3/Ex2.py
+++ Week 3/Ex2.py
@@ -14,6 +14,7 @@ colours = ['r', 'g', 'b'] # Colour Plots
 
 G = 6.672e-11 # Gravitational Constant
 M = 1.989e30 # Solar Mass
+m = 5.97219e24 # Earth Mass
 
 startTime = 0
 endTime = 4e7
@@ -72,25 +73,44 @@ for N in nVals:
     rVals = np.array(rList).transpose()
     xVals = rVals[0]
     yVals = rVals[1]
-    plt.plot(xVals, yVals, 'ro', markersize = 1, label='y(x), RK4')
+    # plt.plot(xVals, yVals, 'ro', markersize = 1, label='y(x), RK4')
     np.delete(rVals, 4)
     
     rList.clear()
     
     # Plot using Verlet Method
     r0 = [x0, y0, vX0, vY0]
+    potentialEnergyVals = list()
+    kineticEnergyVals = list()
+    totalEnergyVals = list()
     for t in tVals:
+        # Calculate GPE, KE and total energy
+        rMag = np.sqrt(r0[0]**2 + r0[1]**2)
+        U = -G*M*m/rMag
+        vMag = np.sqrt(r0[2]**2 + r0[3]**2)
+        T = 1/2 * m * vMag**2
+        E = T + U
+        potentialEnergyVals.append(U); kineticEnergyVals.append(T); totalEnergyVals.append(E)
+        
+        # Calculate pos and vel at new timestep
         r1 = VerletPosition(h, r0, t)
         v1 = VerletVelocity(h, r0, t)
         rList.append(r1)
         r0 = [r1[0], r1[1], v1[0], v1[1]]
+        
     rVals = np.array(rList).transpose()
     xVals = rVals[0]
     yVals = rVals[1]
-    plt.plot(xVals, yVals, 'bo', markersize = 1, label='y(x), Verlet')
+    # plt.plot(xVals, yVals, 'bo', markersize = 1, label='y(x), Verlet')
     np.delete(rVals, 4)
 
-plt.title('Comet Orbit')
+# plt.title('Comet Orbit')
 # plt.legend()
-plt.plot(0, 0, 'o', color='gold')
+# plt.plot(0, 0, 'o', color='gold')
+
+plt.title('Conservation of Energy of Earths Orbit')
+plt.plot(tVals, potentialEnergyVals, 'b', label='GPE')
+plt.plot(tVals, kineticEnergyVals, 'r', label='KE')
+plt.plot(tVals, totalEnergyVals, 'g', label='Total Energy')
+plt.legend()
 plt.show()
