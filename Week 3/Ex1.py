0b0363a89e94a11793085e5c4f6724795cc27664
SamRudgyardBath <sr2116@bath.ac.uk>
2023-02-23 13:06:00 +0000

    Week 3 Ex1 - Use RK4 to calculate flight of ball
    Using RK4, calculate the final displacement (z) at time t=10.

--- Week 3/Ex1.py
+++ Week 3/Ex1.py
@@ -16,3 +16,30 @@ delta = 1
 h = (endTime - startTime)/N
 tVals = np.linspace(startTime, endTime, N)
 rVals = list()
+
+def Velocity(r, t):
+    return r[1]
+
+def Acceleration(r, t):
+    return g
+
+def VectorF(r, t):
+    v = Velocity(r, t)
+    a = Acceleration(r, t)
+    return np.array([v, a])
+
+def FourthOrderRK(h, r0, t):
+    k1 = h*VectorF(r0, t)
+    k2 = h*VectorF(r0 + 0.5*k1, t + 0.5*h)
+    k3 = h*VectorF(r0 + 0.5*k2, t + 0.5*h)
+    k4 = h*VectorF(r0 + k3, t + h)
+    r1 = r0 + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+    return r1
+
+def FindFinalDisp(r0): # Calculate the z displacement of the ball at t=10s
+    rVals.clear()
+    for t in tVals:
+        rVals.append(r0)
+        r1 = FourthOrderRK(h, r0, t)
+        r0 = r1
+    return np.array([r0[0], r0[1]])
